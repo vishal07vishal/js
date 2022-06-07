4a11289a831707d2b6166d5035e782604a204c38
vishal <104211252+vishal07vishal@users.noreply.github.com>
2022-06-07 20:21:58 +0530

    Add files via upload

---
+++ DecisionM.js
@@ -0,0 +1,26 @@
+// const Calculator = (A, B, C ) => {
+//     switch(A){
+//       case '+':
+//         return B+C;
+      
+//       case '-':
+//         return B-C;
+        
+//       case '*':
+//         return B*C;
+        
+//       case '/':
+//         return B/C;
+        
+//       
+//     }
+//     };
+
+//     const Check_divisibility = (N) => {
+//         if(N % 6 === 0 & N % 9 === 0 ){
+//           return ("Divisible by both")
+//         } 
+//         else{ 
+//           return ("Not Divisible by both")
+//         }
+//      };
